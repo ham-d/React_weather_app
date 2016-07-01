6fbd1e6b3763d17378057579687c978680b8c35e
misterrobot <dham001@ucr.edu>
2016-07-01 06:29:52 +0000

    Prep for heroku

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var app = express();
 
+app.use(function(req, res, next){
+    if(req.headers['x-forwarded-proto'] === 'http'){
+        next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 app.use(express.static('public'));
 
 //starting the server
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Server started"); 
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, process.env.IP, function(){
+   console.log("Server started on port: " + PORT); 
 });
