3e4aa4eeb7a3ccb06196a03f5b3ba3c6ca5c855f
misterrobot <dham001@ucr.edu>
2016-07-02 06:48:25 +0000

    add foundation & jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -1,11 +1,15 @@
 var React = require('react');
 var ReactDOM = require("react-dom");
 var { Route, Router, IndexRoute, hashHistory } = require("react-router");
-// var Main = require('./components/Main');
+// var Main = require('./components/Main') but shortcutted through webpack;
 var Main = require('Main');
 var Weather = require("Weather");
 var About = require("About");
 var Example = require("Example");
+
+//load foundation
+require("style!css!foundation-sites/dist/foundation.min.css");
+$(document).foundation();
 
 ReactDOM.render(
     <Router history = {hashHistory}>
